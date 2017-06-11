689c13d1eaff0d5cca0e98884d2dcda3bfb1b1c3
Gabriel <roriz@gmail.com>
2017-06-11 10:35:46 -0300

    models class created

---
+++ TrabalhofinalPOO/src/trabalhofinalpoo/models/Contratado.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhofinalpoo.models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author gabrielroriz
+ */
+public class Contratado extends Corretor{
+    
+    float salarioFixo;
+    
+    Date dataDeAdmissao;
+
+    public float getSalarioFixo() {
+        return salarioFixo;
+    }
+
+    public void setSalarioFixo(float salarioFixo) {
+        this.salarioFixo = salarioFixo;
+    }
+
+    public Date getDataDeAdmissao() {
+        return dataDeAdmissao;
+    }
+
+    public void setDataDeAdmissao(Date dataDeAdmissao) {
+        this.dataDeAdmissao = dataDeAdmissao;
+    }
+    
+}
