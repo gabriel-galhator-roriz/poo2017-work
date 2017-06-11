77372d60a3c67b03796411e62108a6d415e6671a
Gabriel <roriz@gmail.com>
2017-06-11 09:53:31 -0300

    resolved #1

--- TrabalhofinalPOO/src/trabalhofinalpoo/MainScreen.java
+++ TrabalhofinalPOO/src/trabalhofinalpoo/MainScreen.java
@@ -60,7 +60,7 @@ public class MainScreen extends JFrame implements ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e)   {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
